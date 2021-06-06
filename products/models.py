1d5f09519d8c4c0320b57d792d7c812a26310322
nexussummer2021 <nexussummer2021@gmail.com>
2021-06-06 20:59:11 +0530

    added 5 products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(null=False,blank=False,unique=True,max_length=255)
+    price = models.FloatField(null=False,blank=False)
+    
+    def __str__(self):
+        return self.name
